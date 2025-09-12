f73a963f9885de77a9ac175935c3209b8bb334ea
Camila <Camila.leite.oliveira@gmail.com>
2025-09-12 09:44:16 -0300

    Ajuste de estilo

--- login.php
+++ login.php
@@ -5,14 +5,15 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <h2>Login</h2>
-    <form method="post" action="autenticacao.php">
-        <label for="login">Login:</label>
-        <input type="text" id="login" name="login" required>
-        <br>
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required>
-        <br>
-        <input type="submit" value="Login">
-    </form>
+    <div class="login-page">
+        <div class="form">
+            <h2>Login</h2>
+            <form class="login-form" action="autenticacao.php" method="POST">
+                <input type="text" placeholder="login" name="login"/>
+                <input type="password" placeholder="senha" name="senha"/>
+                <button>login</button>
+                <p class="message">Não foi cadastrado? <a href="#">Criar a conta</a></p>
+            </form>
+        </div>
+    </div>
 </body>
